af62667028242979d0eec9920e69e0dfea507b8f
alcides94 <65246102+alcides94@users.noreply.github.com>
2024-04-16 16:58:51 +0200

    se agrega metodo fibonacci en la clase Algoritmos.java

---
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -0,0 +1,40 @@
+package util;
+
+public abstract class Algoritmos {
+	
+	/**
+	 * Devuelve el número de fibonacci de dicho número
+	 * 
+	 * @param 	el numero del que queremos calcular fibonacci
+	 * @return	el fibonacci del numero especificado
+	 */
+	
+	public static int fibonacci (int numero){
+		
+		int fibo=0;
+		
+		if (numero<=0) {
+			return fibo=-1;
+		}
+		else if (numero==1) {
+			
+			return fibo=0;
+		}
+		else if (numero==2) {
+			
+			return fibo=1;
+		}
+		else {
+			int n1=0;
+			int n2=1;
+			
+			for (int i = 2; i < numero; i++){
+				fibo=n1+n2;
+				n1=n2;
+				n2=fibo;	
+			}
+			return fibo;
+		}
+	}
+	
+}
