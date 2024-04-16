87bacb8f622365b3a20cb60d3a4f663798c71c3d
alcides94 <65246102+alcides94@users.noreply.github.com>
2024-04-16 17:04:12 +0200

    se agrega metodo factorial en la clase Algoritmos.java

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -37,4 +37,21 @@ public abstract class Algoritmos {
 		}
 	}
 	
+	/**
+	 * Devuelve el factorial del numero especificado
+	 * 
+	 * @param numero	el numero del que queremos calcular el factorial
+	 * @return			el factorial del numero especificado
+	 */
+	public static int factorial (int numero) {
+		int fact =1;
+		
+		for (int i=1; i<=numero; i++) {
+			fact =fact*i;
+			
+		}
+		
+		return fact;
+	}
+	
 }
